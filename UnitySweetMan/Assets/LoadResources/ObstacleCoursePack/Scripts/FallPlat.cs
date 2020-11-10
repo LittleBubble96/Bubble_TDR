54de6d1a0a213bcf062ed9e44ca48ef158f67e1e
LittleBubble96 <70511362+LittleBubble96@users.noreply.github.com>
2020-11-11 00:08:14 +0800

    增加显示

--- UnitySweetMan/Assets/LoadResources/ObstacleCoursePack/Scripts/FallPlat.cs
+++ UnitySweetMan/Assets/LoadResources/ObstacleCoursePack/Scripts/FallPlat.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FallPlat : MonoBehaviour
 {
 	public float fallTime = 0.5f;
+	public float showTime = 2f;
 
+	private MeshRenderer _selfRenderer;
+	private Collider _collider;
+
+
+	private void Awake()
+	{
+		_selfRenderer = GetComponent<MeshRenderer>();
+		_collider = GetComponent<BoxCollider>();
+	}
 
 	void OnCollisionEnter(Collision collision)
 	{
-		if (SM_SceneManager.Instance.CurLevelData.ELevelState!=ELevelState.Playing)
+		if (SM_SceneManager.Instance.CurLevelData.ELevelState != ELevelState.Playing)
 		{
 			return;
 		}
+
 		foreach (ContactPoint contact in collision.contacts)
 		{
 			//Debug.DrawRay(contact.point, contact.normal, Color.white);
@@ -26,6 +38,15 @@ public class FallPlat : MonoBehaviour
 	IEnumerator Fall(float time)
 	{
 		yield return new WaitForSeconds(time);
-		Destroy(gameObject);
+		EnableRender(false);
+		yield return new WaitForSeconds(showTime);
+		EnableRender(true);
 	}
+
+	void EnableRender(bool enable)
+	{
+		_selfRenderer.enabled = enable;
+		_collider.enabled = enable;
+	}
+
 }
